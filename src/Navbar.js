2e02932a7d0fa4cd47cf03a8adc871fb9b9649cb
Walid Daniel Dib <112762655+w-dib@users.noreply.github.com>
2022-12-05 00:26:44 +0400

    Work on the API get using Axios

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,7 +1,18 @@
 import React from 'react'
 import MTGLogo from "./images/MTG-Logo.png";
-
+import Axios from "axios"
 export default function Navbar() {
+
+
+  const [card, setNewCard] = React.useState("")  
+  const getCard = () =>{
+    Axios.get("https://api.magicthegathering.io/v1/cards").then(
+        (response)=>{
+            setNewCard(response.data.cards[0].imageUrl)
+        }
+    )
+  }   
+
   return (
     <div><nav className="navbar navbar-light bg-light">
     <div className="container-fluid">
@@ -12,6 +23,8 @@ export default function Navbar() {
       </form>
     </div>
   </nav>
+  <button onClick={getCard}>Click</button>
+  <img src={card}></img>
   </div>
   )
 }
